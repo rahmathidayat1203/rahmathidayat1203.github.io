97ae776100cae3f5a8ea7316c8932bb6cc3a21e9
Rahmat12-web <rh63800@gmail.com>
2024-10-16 02:15:16 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// Menu toggle functionality
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+const typingText = document.getElementById('typing-text');
+const phrases = ['Web Developer', 'Android Developer', 'IOT Developer'];
+let phraseIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentPhrase = phrases[phraseIndex];
+
+    if (isDeleting) {
+        typingText.textContent = currentPhrase.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        typingText.textContent = currentPhrase.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    if (!isDeleting && charIndex === currentPhrase.length) {
+        isDeleting = true;
+        setTimeout(type, 1500);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        setTimeout(type, 500);
+    } else {
+        setTimeout(type, isDeleting ? 50 : 150);
+    }
+}
+
+type();
